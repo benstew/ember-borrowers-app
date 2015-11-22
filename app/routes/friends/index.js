24f06e476ced0bc575367863c0c871ad4f6cd54f
benstew <benstewart2008@gmail.com>
2015-11-22 09:24:14 -0500

    Refactored delete functionality for friends/show and friends/index to inherit directly from the parent friend route

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -3,11 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({ 
   model() {
     return this.store.findAll('friend'); 
-  },
-  actions: { 
-    delete(friend) {
-      friend.destroyRecord();
-      return false; 
-    }
-  } 
+  }
 });
+
+
+// Abstracted away with delete functionality on the friends route
+// export default Ember.Route.extend({ 
+//   model() {
+//     return this.store.findAll('friend'); 
+//   },
+//   actions: { 
+//     delete(friend) {
+//       friend.destroyRecord();
+//       return false; 
+//     }
+//   } 
+// });
--- app/routes/friends/show.js
+++ app/routes/friends/show.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({ 
-  actions: { 
-    delete(friend) {
-      friend.destroyRecord().then(() => {
-        this.transitionTo('friends.index');
-      });
-    }
-  } 
-});
+export default Ember.Route.extend({ });
+
+
+// Abstracted away with delete action on friends route
+// import Ember from 'ember';
+
+// export default Ember.Route.extend({ 
+//   actions: { 
+//     delete(friend) {
+//       friend.destroyRecord().then(() => {
+//         this.transitionTo('friends.index');
+//       });
+//     }
+//   } 
+// });
